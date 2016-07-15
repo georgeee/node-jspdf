7306409b1e1eba0bc6b0aa2bedf6ad24d59f172e
George Agapov <george.agapov@gmail.com>
2016-07-15 22:30:48 +0300

    Compatibility fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var path = './vendor/jsPDF/';
-jsPDF = require(path+'jspdf');
+var jsPDFVendorPath = './vendor/jsPDF/';
+jsPDF = require(jsPDFVendorPath+'jspdf');
 
 //Load all the plugins
 var plugins = ['addhtml', 'addimage', 'annotations', 'autoprint', 'cell', 'context2d', 'from_html', 'javascript', 'outline', 'png_support', 'split_text_to_size', 'standard_fonts_metrics', 'svg', 'total_pages'];
 plugins.map(function(plugin){
-    require(path+'/plugins/'+plugin+'.js');
+    require(jsPDFVendorPath+'/plugins/'+plugin+'.js');
 });
 
 //Modify the save function to save to disk
